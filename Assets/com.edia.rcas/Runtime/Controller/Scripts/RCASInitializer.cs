d0ab661f2f9ec390c4e0f457557896f0c1b1906a
Jeroen De mooij <jeroen@the1stfloor.net>
2025-02-07 16:46:31 +0100

    Resources.load prefab instead of link in inspector

--- Assets/com.edia.rcas/Runtime/Controller/Scripts/RCASInitializer.cs
+++ Assets/com.edia.rcas/Runtime/Controller/Scripts/RCASInitializer.cs
@@ -3,13 +3,12 @@ using UnityEngine;
 
 public class RCASInitializer : MonoBehaviour
 {
-    public GameObject PairingPanelPrefab = null;
-
     private void Awake() {
         // Set controller of this scene to remote
         Edia.Controller.ControlPanel.Instance.ControlMode = Edia.Constants.ControlModes.Remote;
-        
+            
         // Instantiate the pairingpanel
-        GameObject pairingPanel = Instantiate(PairingPanelPrefab, ControlPanel.Instance.NonActivePanelHolder.transform);
+        GameObject pairingPanel = Resources.Load ("Panel-PairingOffer") as GameObject;
+        Instantiate(pairingPanel, ControlPanel.Instance.NonActivePanelHolder.transform);
     }
 }
